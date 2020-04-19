9235c071c4b1a56e7ae2b8bd8f3a193cc76db695
Seamus Riordan <seamus@riordan.io>
2020-04-19 07:05:07 -0500

    Propagating is based on rng

--- propagator.py
+++ propagator.py
@@ -1,9 +1,12 @@
+from random import random
+
+
 class Propagator:
     def __init__(self, people, rate=0.0) -> None:
         self.people = people
         self.rate = rate
 
     def step(self):
-        if self.rate > 0.5:
+        if self.rate > random():
             for person in self.people:
                 person.infected = True
--- test_propagator.py
+++ test_propagator.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from person import Person
 from propagator import Propagator
@@ -12,7 +12,7 @@ class Test(TestCase):
         self.two_in_contact[0].contacts.append(self.two_in_contact[1])
         self.two_in_contact[1].contacts.append(self.two_in_contact[0])
 
-    def test_propagator_step__with_two_in_contact_and_zero_rate_does_nothing(self):
+    def test_propagator_step_with_two_in_contact_and_zero_rate_does_nothing(self):
         propagator = Propagator(self.two_in_contact, rate=0.0)
 
         propagator.step()
@@ -20,12 +20,20 @@ class Test(TestCase):
         self.assertFalse(self.two_in_contact[0].infected)
         self.assertFalse(self.two_in_contact[1].infected)
 
-    def test_propagator_step__with_two_in_contact_and_one_infected_with_unity_rate_infects_other(self):
+    def test_propagator_step_one_infected_unity_rate_infects_other(self):
         self.two_in_contact[0].infected = True
-
         propagator = Propagator(self.two_in_contact, rate=1.0)
 
         propagator.step()
 
-        self.assertTrue(self.two_in_contact[0].infected)
+        self.assertTrue(self.two_in_contact[1].infected)
+
+    @mock.patch('random.random')
+    def test_propagator_step_half_rate_infects_other_with_rng_zero(self, mock_random):
+        mock_random.return_value = 0.0
+        self.two_in_contact[0].infected = True
+        propagator = Propagator(self.two_in_contact, rate=0.5)
+
+        propagator.step()
+
         self.assertTrue(self.two_in_contact[1].infected)
